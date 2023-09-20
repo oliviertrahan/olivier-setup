23cdc1ee5fdd4d8116e93c2ab2dfe37f0757ee10
Olivier Trahan <trahan.olivier@gmail.com>
2023-09-19 21:31:11 -0400

    yo

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,15 @@
 local telescope = require('telescope')
+local actions = require('telescope.actions')
+
 telescope.setup {
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-j>"] = actions.move_selection_next
+            }
+        }
+    },
     pickers = {
         find_files = {
             hidden = true
@@ -13,7 +23,7 @@ vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
