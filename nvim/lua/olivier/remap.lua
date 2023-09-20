15726d9f4c13586e23f88046403f6ee11eb61546
Olivier Trahan <trahan.olivier@gmail.com>
2023-09-19 21:36:33 -0400

    not necessary

--- nvim/lua/olivier/remap.lua
+++ nvim/lua/olivier/remap.lua
@@ -26,7 +26,6 @@ vim.keymap.set("n", "<leader>wl", "<C-w>l")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
-vim.keymap.set("n", "<leader>ot", "<cmd>terminal<CR>")
 
 --better tab management
 vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")
