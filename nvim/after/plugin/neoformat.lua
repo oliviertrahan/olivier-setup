fb6ae8fb7341f1b1c24fca556a406458d7b6cd31
Olivier Trahan <trahan.olivier@gmail.com>
2023-10-23 15:26:03 -0400

    dont autoformat for *.cs

--- nvim/after/plugin/neoformat.lua
+++ nvim/after/plugin/neoformat.lua
@@ -3,7 +3,7 @@ vim.g.neoformat_try_node_exe = 1
 vim.api.nvim_create_autocmd(
     'BufWritePre,InsertLeavePre',
     {
-        pattern='*.vue, *.cs',
+        pattern='*.vue',
         command='Neoformat'
     }
 )
