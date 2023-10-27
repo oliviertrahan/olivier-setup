60b88e755f2e71d439775c1da9f768dd7fd96a0f
Olivier Trahan <trahan.olivier@gmail.com>
2023-10-27 00:31:05 -0400

    autoformat fixes

--- nvim/after/plugin/autosession.lua
+++
@@ -1,9 +0,0 @@
-
--- vim.cmd [[
---     set sessionoptions+=localoptions
---     autocmd BufWinEnter * windo filetype detect
---     autocmd SessionLoadPost * windo filetype detect
---     autocmd VimEnter * so ~/Session.vim
---     autocmd SessionLoadPost * NvimTreeCollapse
---     autocmd VimLeave * mks! ~/Session.vim
--- ]]
