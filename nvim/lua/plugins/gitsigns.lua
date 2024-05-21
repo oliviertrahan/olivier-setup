2a7dc42fee223287b57fe68d3eafa9fdb43894a6
olivier-rvezy <olivier@rvezy.com>
2024-05-21 11:15:00 -0400

    gitsigns + lsp

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,15 +1,16 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    lazy = vim.g.started_by_firenvim,
-    config = function()
-        if vim.g.started_by_firenvim then
-          return
-        end
-        require("gitsigns").setup()
+	"lewis6991/gitsigns.nvim",
+	lazy = vim.g.started_by_firenvim,
+	config = function()
+		if vim.g.started_by_firenvim then
+			return
+		end
+		require("gitsigns").setup()
 
-        vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns next_hunk<CR>", { noremap = true, silent = true })
-        vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true })
-    end
+		vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns next_hunk<CR>", { noremap = true, silent = true })
+		-- vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns toggle_current_line_blame<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>gt", "<cmd>Gitsigns blame_line<CR>", { noremap = true, silent = true })
+	end,
 }
