884e9974a9717d6d7ebd9d42383a9ad150829b30
olivier-rvezy <olivier@rvezy.com>
2024-05-25 00:21:32 -0400

    telescope tabs config better, treesitter more text objects

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,31 +2,31 @@ require("setup.set")
 require("setup.remap")
 require("setup.lazy")
 require("setup.snippets")
-
+vim.g.tab_names = {}
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+local yank_group = augroup("HighlightYank", {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+	require("plenary.reload").reload_module(name)
 end
 
 -- Highlight yanked text
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
 })
 
 -- When we leave terminal mode or when we refocus neovim, then check files for changes
-autocmd({ 'FocusGained', 'TermLeave' }, {
-    pattern = '*',
-    callback = function()
-        vim.cmd('checktime')
-    end,
+autocmd({ "FocusGained", "TermLeave" }, {
+	pattern = "*",
+	callback = function()
+		vim.cmd("checktime")
+	end,
 })
--- nvim/lua/setup/remap.lua
+++ nvim/lua/setup/remap.lua
@@ -1,4 +1,4 @@
-local remap_funcs = require('setup.remap_functions')
+local remap_funcs = require("setup.remap_functions")
 local set_print_snippet = remap_funcs.set_print_snippet
 local open_project_terminal = remap_funcs.open_project_terminal
 local open_terminal = remap_funcs.open_terminal
@@ -14,54 +14,46 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "yA", "mzgg0VG$y`z")                     -- yank the whole document
-vim.keymap.set("n", "=A", "mzgg0VG$=`z")                     -- format the whole document
-vim.keymap.set("n", "Q", "@q")                               --Execute "q" macro with Q
+vim.keymap.set("n", "yA", "mzgg0VG$y`z") -- yank the whole document
+vim.keymap.set("n", "=A", "mzgg0VG$=`z") -- format the whole document
+vim.keymap.set("n", "Q", "@q") --Execute "q" macro with Q
 vim.keymap.set("x", "Q", ":norm @q<CR>", { noremap = true }) --Execute "q" macro with Q
-vim.keymap.set("n", "x", "\"_dl")                            --delete single character doesn't mess with yank register
-vim.keymap.set("n", "gb", "<C-6>")                           -- go back to last file
+vim.keymap.set("n", "x", '"_dl') --delete single character doesn't mess with yank register
+vim.keymap.set("n", "gb", "<C-6>") -- go back to last file
 vim.keymap.set("n", "<leader>pp", function()
-    local path = vim.fn.expand('%')
-    vim.fn.setreg("+", path)
-    vim.notify('Copied "' .. path .. '" to the clipboard!')
+	local path = vim.fn.expand("%")
+	vim.fn.setreg("+", path)
+	vim.notify('Copied "' .. path .. '" to the clipboard!')
 end)
 
--- function YankWithoutMoving()
---     local save_cursor = vim.api.nvim_win_get_cursor(0)
---     vim.cmd([[normal! `[v`]y]])
---     vim.api.nvim_win_set_cursor(0, save_cursor)
--- end
--- -- Create a mapping for yanking without moving the cursor
--- vim.keymap.set('n', 'y', ':lua YankWithoutMoving()<CR>', { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>ss", "yiw:%s/<C-r>\"//g<Left><Left>") -- go back to last file
-vim.keymap.set("v", "<leader>ss", "y:%s/<C-r>\"//g<Left><Left>")   -- go back to last file
+vim.keymap.set("n", "<leader>ss", 'yiw:%s/<C-r>"//g<Left><Left>') -- go back to last file
+vim.keymap.set("v", "<leader>ss", 'y:%s/<C-r>"//g<Left><Left>') -- go back to last file
 vim.keymap.set("n", "<leader>dd", create_debug_buffer)
 vim.keymap.set("n", "/", "/\\c") -- Case Insensitive search
 vim.keymap.set("n", "?", "?\\c") -- Case Insensitive search
 vim.keymap.set("n", "<leader>qh", "<cmd>colder<CR>") -- Previous quickfix list
-vim.keymap.set("n", "<leader>ql", "<cmd>cnewer<CR>") -- Next quickfix list 
+vim.keymap.set("n", "<leader>ql", "<cmd>cnewer<CR>") -- Next quickfix list
 
 -- create new line without going into insert mode
-vim.keymap.set("n", "<leader>O", "Oi<ESC>\"_dl", { noremap = false })
-vim.keymap.set("n", "<leader>o", "oi<ESC>\"_dl", { noremap = false })
+vim.keymap.set("n", "<leader>O", 'Oi<ESC>"_dl', { noremap = false })
+vim.keymap.set("n", "<leader>o", 'oi<ESC>"_dl', { noremap = false })
 
 -- better visual experience
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { noremap = true }) -- Move visually selected text one line up
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { noremap = true }) -- Move visually selected text one line down
-vim.keymap.set("v", "x", "<Esc>", { noremap = true })            --x to exit visual mode easily
-vim.keymap.set("v", "y", "ygv<Esc>", { noremap = true })         --yank won't move cursor anymore
-vim.keymap.set("v", "A", "mzgg0oG$", { noremap = true })         -- visual select everything
-vim.keymap.set("v", "H", "Hzz", { noremap = true })              --move up doc easily
-vim.keymap.set("v", "L", "Lzz", { noremap = true })              --move down doc easily
-vim.keymap.set("v", ">", ">gv", { noremap = true })              --continue visually selecting after indenting
-vim.keymap.set("v", "<", "<gv", { noremap = true })              --continue visually selecting after indenting
-vim.keymap.set("v", "Y", "\"+ygv<Esc>", { noremap = true })      --Y to copy to system clipboard
-vim.keymap.set("v", "d", "\"0d", { noremap = true })            -- make delete in visual mode go to yank register so I can paste it later
-vim.keymap.set("v", "p", "\"0p", { noremap = true })            --pasting over selected text doesn't delete what is in yank buffer
-vim.keymap.set("v", "P", "\"+p", { noremap = true })         	--pasting over selected text doesn't delete what is in yank buffer
-vim.keymap.set("v", "il", "<Esc>^v$h", { noremap = true })       --visually select whole line but not EOL char
-vim.keymap.set("v", "iL", "<Esc>^v$h", { noremap = true })       --visually select whole line but not EOL char
+vim.keymap.set("v", "x", "<Esc>", { noremap = true }) --x to exit visual mode easily
+vim.keymap.set("v", "y", "ygv<Esc>", { noremap = true }) --yank won't move cursor anymore
+vim.keymap.set("v", "A", "mzgg0oG$", { noremap = true }) -- visual select everything
+vim.keymap.set("v", "H", "Hzz", { noremap = true }) --move up doc easily
+vim.keymap.set("v", "L", "Lzz", { noremap = true }) --move down doc easily
+vim.keymap.set("v", ">", ">gv", { noremap = true }) --continue visually selecting after indenting
+vim.keymap.set("v", "<", "<gv", { noremap = true }) --continue visually selecting after indenting
+vim.keymap.set("v", "Y", '"+ygv<Esc>', { noremap = true }) --Y to copy to system clipboard
+vim.keymap.set("v", "d", '"0d', { noremap = true }) -- make delete in visual mode go to yank register so I can paste it later
+vim.keymap.set("v", "p", '"0p', { noremap = true }) --pasting over selected text doesn't delete what is in yank buffer
+vim.keymap.set("v", "P", '"+p', { noremap = true }) --pasting over selected text doesn't delete what is in yank buffer
+vim.keymap.set("v", "il", "<Esc>^v$h", { noremap = true }) --visually select whole line but not EOL char
+vim.keymap.set("v", "iL", "<Esc>^v$h", { noremap = true }) --visually select whole line but not EOL char
 
 -- better insert experience
 vim.keymap.set("i", "<C-h>", "<C-o>h", { noremap = true })
@@ -95,45 +87,45 @@ vim.keymap.set("n", "<leader>tN", "<cmd>-tabmove<CR>")
 
 -- print snippets in multiple languages
 
-set_print_snippet(true, false, "vim.print(\"", ": \" .. ", ")")
-set_print_snippet(false, false, "vim.print(\"", ": \" .. ", ")")
-set_print_snippet(true, true, "vim.print(\"", ": \" .. dump(", "))")
-set_print_snippet(false, true, "vim.print(\"", ": \" .. dump(", "))")
+set_print_snippet(true, false, 'vim.print("', ': " .. ', ")")
+set_print_snippet(false, false, 'vim.print("', ': " .. ', ")")
+set_print_snippet(true, true, 'vim.print("', ': " .. dump(', "))")
+set_print_snippet(false, true, 'vim.print("', ': " .. dump(', "))")
 
-local snippet_group = vim.api.nvim_create_augroup('augroup', {})
+local snippet_group = vim.api.nvim_create_augroup("augroup", {})
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd('BufEnter', {
-    group = snippet_group,
-    pattern = '*.cs',
-    callback = function(ev)
-        set_print_snippet(true, false, "Console.WriteLine($\"", ": {", "}\");", ev.buf)
-        set_print_snippet(false, false, "Console.WriteLine($\"", ": {", "}\");", ev.buf)
-        set_print_snippet(true, true, "Console.WriteLine($\"", ": {JsonConvert.SerializeObject(", ")}\");", ev.buf)
-        set_print_snippet(false, true, "Console.WriteLine($\"", ": {JsonConvert.SerializeObject(", ")}\");", ev.buf)
-    end,
+autocmd("BufEnter", {
+	group = snippet_group,
+	pattern = "*.cs",
+	callback = function(ev)
+		set_print_snippet(true, false, 'Console.WriteLine($"', ": {", '}");', ev.buf)
+		set_print_snippet(false, false, 'Console.WriteLine($"', ": {", '}");', ev.buf)
+		set_print_snippet(true, true, 'Console.WriteLine($"', ": {JsonConvert.SerializeObject(", ')}");', ev.buf)
+		set_print_snippet(false, true, 'Console.WriteLine($"', ": {JsonConvert.SerializeObject(", ')}");', ev.buf)
+	end,
 })
 
-autocmd('BufEnter', {
-    group = snippet_group,
-    pattern = { '*.js', '*.mjs', '*.cjs', '*.vue' },
-    callback = function(ev)
-        set_print_snippet(true, false, "console.log(`", ": ${", "}`)", ev.buf)
-        set_print_snippet(false, false, "console.log(`", ": ${", "}`)", ev.buf)
-        set_print_snippet(true, true, "console.log(`", ": ${JSON.stringify(", ")}`)", ev.buf)
-        set_print_snippet(false, true, "console.log(`", ": ${JSON.stringify(", ")}`)", ev.buf)
-    end,
+autocmd("BufEnter", {
+	group = snippet_group,
+	pattern = { "*.js", "*.mjs", "*.cjs", "*.vue" },
+	callback = function(ev)
+		set_print_snippet(true, false, "console.log(`", ": ${", "}`)", ev.buf)
+		set_print_snippet(false, false, "console.log(`", ": ${", "}`)", ev.buf)
+		set_print_snippet(true, true, "console.log(`", ": ${JSON.stringify(", ")}`)", ev.buf)
+		set_print_snippet(false, true, "console.log(`", ": ${JSON.stringify(", ")}`)", ev.buf)
+	end,
 })
 
-autocmd('BufEnter', {
-    group = snippet_group,
-    pattern = { '*.py' },
-    callback = function(ev)
-        set_print_snippet(true, false, "print(f\"", ": {", "}\")", ev.buf)
-        set_print_snippet(false, false, "print(f\"", ": {", "}\")", ev.buf)
-        set_print_snippet(true, true, "print(f\"", ": {json.dumps(", ")}\")", ev.buf)
-        set_print_snippet(false, true, "print(f\"", ": {json.dumps(", ")}\")", ev.buf)
-    end,
+autocmd("BufEnter", {
+	group = snippet_group,
+	pattern = { "*.py" },
+	callback = function(ev)
+		set_print_snippet(true, false, 'print(f"', ": {", '}")', ev.buf)
+		set_print_snippet(false, false, 'print(f"', ": {", '}")', ev.buf)
+		set_print_snippet(true, true, 'print(f"', ": {json.dumps(", ')}")', ev.buf)
+		set_print_snippet(false, true, 'print(f"', ": {json.dumps(", ')}")', ev.buf)
+	end,
 })
 
 --Terminal mode improvement
@@ -141,16 +133,26 @@ vim.keymap.set("n", "<C-t>", open_project_terminal)
 vim.keymap.set("t", "<C-t>", "<C-\\><C-n><C-w>c")
 vim.keymap.set("t", "<C-a>", "<C-\\><C-n>")
 vim.keymap.set("t", "<C-p>", "<C-\\><C-o>p")
-vim.keymap.set("t", "<C-v>", "<C-\\><C-o>\"+P")
+vim.keymap.set("t", "<C-v>", '<C-\\><C-o>"+P')
 vim.keymap.set("t", "<C-o>", "<C-\\><C-o>")
 
 --better terminal management
-vim.keymap.set("n", "<leader>t1", function() open_terminal(1) end)
-vim.keymap.set("n", "<leader>t2", function() open_terminal(2) end)
-vim.keymap.set("n", "<leader>t3", function() open_terminal(3) end)
-vim.keymap.set("n", "<leader>t4", function() open_terminal(4) end)
-vim.keymap.set("n", "<leader>t5", function() open_terminal(5) end)
+vim.keymap.set("n", "<leader>t1", function()
+	open_terminal(1)
+end)
+vim.keymap.set("n", "<leader>t2", function()
+	open_terminal(2)
+end)
+vim.keymap.set("n", "<leader>t3", function()
+	open_terminal(3)
+end)
+vim.keymap.set("n", "<leader>t4", function()
+	open_terminal(4)
+end)
+vim.keymap.set("n", "<leader>t5", function()
+	open_terminal(5)
+end)
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+	vim.cmd("so")
 end)
