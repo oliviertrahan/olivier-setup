7e0599fbbbb12e166b507f6a950e930ec4b76bd1
Olivier Trahan <trahan.olivier@gmail.com>
2024-06-13 08:20:05 -0400

    codesnap plhgin added

---
+++ nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,14 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make build_generator",
+  keys = {
+    { "<leader>csc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>css", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  },
+  opts = {
+    save_path = "~/CodeSnap",
+    has_breadcrumbs = true,
+    watermark = "",
+    bg_theme = "grape",
+  },
+}
