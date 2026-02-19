63a07f51bf852e13ffba2dac1635bd8c59d3e64a
--replace-all <user.name>
2026-02-19 15:28:10 -0500

    common bashrc fix

--- common_bashrc.sh
+++ common_bashrc.sh
@@ -18,7 +18,7 @@ case "${unameOut}" in
 esac
 
 # All unix path styles should be bash compatible
-if [ "$pathStyle" == "unix" ]; then
+if [ "$pathStyle" = "unix" ]; then
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
